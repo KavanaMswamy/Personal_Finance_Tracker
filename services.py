882e13acac930c7b8775596525e8a7d1c8720df6
kavanakm <kavanamswamy20@gmail.com>
2026-06-10 02:06:50 +0400

    Created seperate file for business logic to maintain the clean flow

---
+++ services.py
@@ -0,0 +1,42 @@
+transactions=[]
+
+def add_transactions():
+    amount=float(input("Enter amount->"))
+    category=input("enter category->")
+    type=input("Enter Income/Expences->")
+    #creating a dictionary
+    transaction={
+        "Amount":amount,
+        "Category":category,
+        "Input type":type}
+        
+    transactions.append(transaction)
+
+def view_transactions():
+    if len(transactions)==0:
+        print("No transactions found")
+        return
+    print("Viewing Transactions")
+    for transaction in transactions: #using the loop to take the dictionaries out of loop and print it seperately
+    #print(transactions) # directly printing the list
+        print("Amount : ",transaction["Amount"])
+        print("Category : ",transaction["Category"])
+        print("Input : ",transaction["Input type"])
+        print("---------------------")   
+
+def show_summary():
+    if len(transactions)==0:
+        print("No transactions found")
+        return
+    print("Displaying the summary")
+    total_income=0
+    total_expence=0
+    for transaction in transactions:
+        if transaction["Input type"].lower()=="income":
+            total_income+=transaction["Amount"]
+        else:
+            total_expence+=transaction["Amount"]
+
+    print("Income=",total_income)
+    print("Expence=",total_expence)
+    print("Balance=",total_income-total_expence)
